a4fadd199aff817a9ff0c26df1149432c805fbd8
Mohamed Amr <mohamed,amr@avelabs.com>
2019-11-15 09:16:09 +0200

    This is my First Commit Ever

---
+++ GuessTheWord.h
@@ -0,0 +1,10 @@
+#ifndef _GUESSTHEWORD_H
+#define _GUESSTHEWORD_H
+
+
+/*please call this function in the main*/
+
+void GuessTheWord_Operation(void);
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,13 @@
+#include "GuessTheWord.h"
+
+
+int main(void)
+{
+
+	
+GuessTheWord_Operation();
+	
+while(1);	
+	
+	
+}	
